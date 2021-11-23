3c8e81eda23d88371a9d787c20cf3b9363cf9951
taiki yamashita <taikity0808@gmail.com>
2021-11-23 14:31:22 +0900

    feature: FindByIdメソッドを作成

--- interfaces/database/user_repository.go
+++ interfaces/database/user_repository.go
@@ -15,11 +15,30 @@ func (repo *UserRepository) Store(u domain.User) (id int, err error) {
 	if err != nil {
 		return
 	}
-	
+	// LastInsertIdメソッドで最後に保存されたidを取得している？
 	id64, err := result.LastInsertId()
 	if err != nil {
 		return
 	}
 	id = int(id64)
+	return
+}
+
+func (repo *UserRepository) FindById(identifier int) (user domain.User, err error) {
+	row, err := repo.Query("SELECT id, first_name, last_name FROM users WHERE id = ?", identifier)
+	defer row.Close()
+	if err != nil {
+		return
+	}
+	var id int
+	var firstName string
+	var lastName string
+	row.Next()
+	if err = row.Scan(&id, &firstName, &lastName); err != nil {
+		return
+	}
+	user.ID = id
+	user.FirstName = firstName
+	user.LastName = lastName
 	return
 }
