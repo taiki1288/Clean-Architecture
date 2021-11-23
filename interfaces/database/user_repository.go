96e01d42d547dba4efa714ffaefdeb18a3186197
taiki yamashita <taikity0808@gmail.com>
2021-11-23 10:59:38 +0900

    feature: Storeメソッドを作成

--- interfaces/database/user_repository.go
+++ interfaces/database/user_repository.go
@@ -1,5 +1,25 @@
 package database
 
+// これは内側に依存しているので依存関係に関してはOK
+import "Clean-Architecture/domain"
+
 type UserRepository struct {
 	SqlHandler
+}
+
+func (repo *UserRepository) Store(u domain.User) (id int, err error) {
+	// 実行したクエリの結果を渡している。
+	result, err := repo.Execute(
+		"INSERT INTO users (first_name, last_name) VALUES (?, ?)", u.FirstName, u.LastName,
+	)
+	if err != nil {
+		return
+	}
+	
+	id64, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+	id = int(id64)
+	return
 }
