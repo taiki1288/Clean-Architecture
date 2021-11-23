e812c4c0f8bdfe37e0b5cc0fc316bd2383a42e8e
taiki yamashita <taikity0808@gmail.com>
2021-11-23 09:31:49 +0900

    feature: Resultインターフェースを作成

--- interfaces/database/sql_handler.go
+++ interfaces/database/sql_handler.go
@@ -4,3 +4,8 @@ type SqlHandler interface {
 	Execute(string, ...interface{}) (Result, error)
 	Query(string ...interface{}) (Row, error)
 }
+
+type Result interface{
+	LastInsertId() (int64, error)
+	RowsAffected() (int64, error)
+}
