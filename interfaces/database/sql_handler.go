d01942457bd0fd503913f77078b24be8f36d4429
taiki yamashita <taikity0808@gmail.com>
2021-11-23 09:46:39 +0900

    add: コメントを追加

--- interfaces/database/sql_handler.go
+++ interfaces/database/sql_handler.go
@@ -1,15 +1,19 @@
 package database
 
+// database/user_repository.goでこれを呼び出す。
 type SqlHandler interface {
+	// 2つのメソッドの戻り値はResultとRowのインターフェースになっているのでこれによって依存ルールを守ることができる。
 	Execute(string, ...interface{}) (Result, error)
 	Query(string ...interface{}) (Row, error)
 }
 
+// Executeの戻り値
 type Result interface{
 	LastInsertId() (int64, error)
 	RowsAffected() (int64, error)
 }
 
+// Queryの戻り値
 type Row interface {
 	Scan(...interface{}) error
 	Next() bool
