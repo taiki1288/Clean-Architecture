f34a340ec704446b6f00f2c9b28f8bd40d1b5805
taiki yamashita <taikity0808@gmail.com>
2021-11-24 23:24:37 +0900

    feature: NewUserControllerメソッドを作成

--- interfaces/controllers/user_controller.go
+++ interfaces/controllers/user_controller.go
@@ -1,7 +1,20 @@
 package controllers
 
-import "Clean-Architecture/usecase"
+import (
+	"Clean-Architecture/interfaces/database"
+	"Clean-Architecture/usecase"
+)
 
 type UserController struct {
 	Interactor usecase.UserInteractor
+}
+
+func NewUserController(sqlHandler database.SqlHandler) *UserController {
+	return &UserController{
+		Interactor: usecase.UserInteractor{
+			UserRepository: &database.UserRepository{
+				SqlHandler: sqlHandler,
+			},
+		},
+	}
 }
