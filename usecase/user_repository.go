c7a715e4d01c35ee968e9cffc69353f77bfff33a
taiki yamashita <taikity0808@gmail.com>
2021-11-24 19:29:04 +0900

    feature: UserByIdメソッドを作成

--- usecase/user_repository.go
+++ usecase/user_repository.go
@@ -5,5 +5,5 @@ import "Clean-Architecture/domain"
 type UserRepository interface {
 	Store(domain.User) (int, error)
 	FindById(int) (domain.User, error)
-	FindAll() (domain.User, error)
+	FindAll() (domain.Users, error)
 }
