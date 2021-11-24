f3a91d912a560bfb1f932cabe42581be976936b7
taiki yamashita <taikity0808@gmail.com>
2021-11-24 20:06:43 +0900

    add: comment

--- usecase/user_repository.go
+++ usecase/user_repository.go
@@ -2,6 +2,7 @@ package usecase
 
 import "Clean-Architecture/domain"
 
+// database層は外側だから、ここでもDIPを利用して依存ルールを守る。
 type UserRepository interface {
 	Store(domain.User) (int, error)
 	FindById(int) (domain.User, error)
