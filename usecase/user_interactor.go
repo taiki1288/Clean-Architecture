73f44c754735f9ed5e7d3491f34e89f3b8d4aaea
taiki yamashita <taikity0808@gmail.com>
2021-11-24 15:06:12 +0900

    Usersメソッドを定義

--- usecase/user_interactor.go
+++ usecase/user_interactor.go
@@ -10,3 +10,8 @@ func (interactor *UserInteractor) Add(u domain.User) (err error) {
 	_, err := interactor.UserRepository.Store(u)
 	return
 }
+
+func (interactor *UserInteractor) Users() (user domain.Users, err error) {
+	user, err = interactor.UserRepository.FindAll()
+	return
+}
